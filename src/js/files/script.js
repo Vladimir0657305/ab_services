da16ea1ade997541dc50e4357abc440b308f8457
Vladimir0657305 <vv0657305@gmail.com>
2022-10-12 20:39:04 +0300

    newchanges

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,7 +3,7 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
-// Burger Button animation
+// Burger Button animation =========================================================================
 const buttons = document.querySelectorAll('button');
 buttons.forEach(btn => {
     btn.addEventListener('click', function (e) {
@@ -22,10 +22,21 @@ buttons.forEach(btn => {
     });
 });
 
-// Burger
+// Burger ========================================================================================
 const burg = document.querySelector('.toggle-menu');
 burg.onclick = (() => {
     burg.classList.toggle('active');
     document.querySelector('.header-nav').classList.toggle('open');
     // document.body.classList.toggle('._lock');
 })
+
+//parallax effect for banner =====================================================================
+window.onscroll = function () { paralax() };
+let posY;
+let image = document.getElementById('parallax');
+let parallaxSpan = document.getElementById('parallaxSpan');
+function paralax() {
+    posY = window.pageYOffset;
+    image.style.top = posY * .5 + 'px';
+    parallaxSpan.style.top = posY * .2 + 'px';
+}
